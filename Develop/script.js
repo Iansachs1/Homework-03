f2a29de4aa6f99a0251beb4e1143507a16aaac0e
Iansachs1 <iansachs1@gmail.com>
2020-07-21 19:03:52 -0700

    I forgot to regularly push so... added all javascript and psuedocode in readme

---
+++ Develop/script.js
@@ -0,0 +1,88 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var passwordDisplay = document.getElementById("password");
+
+const lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const numbers = "0123456789".split("");
+const specialCharacters = "! #$%&'()*+,-./:;<>=?@{}^_{}|~`".split("");
+let passwordOptions;
+let passwordLength;
+
+// Write password to the #password input
+
+function setPasswordLength() {
+
+  passwordLength = parseInt(prompt("Pick a password length between 8 and 128 characters."));
+
+  while (passwordLength < 8 || passwordLength > 128) {
+    let passwordLength = parseInt(prompt("Pick a password length between 8 and 128 characters."));
+    passwordLength = alert("please pick a valid password length between 8 and 128 characters");
+  };
+};
+
+function defineCharacterTypes() {
+  passwordOptions = [];
+  useLowerCase  = confirm("Include lower case letters?");
+  useUpperCase  = confirm("Include upper case letters?");
+  useNumbers  = confirm("Include numbers?");
+  useSpecialCharacters  = confirm("Include special characters?");
+
+  while(useLowerCase === false && useUpperCase === false && useNumbers === false && useSpecialCharacters === false) {
+
+    alert("Password must contain at least one type of character.");
+
+    useLowerCase  = confirm("Include lower case letters?");
+    useUpperCase  = confirm("Include upper case letters?");
+    useNumbers  = confirm("Include numbers?");
+    useSpecialCharacters  = confirm("Include special characters?");
+  };
+
+  if (useLowerCase === true) {
+    passwordOptions.push.apply(passwordOptions, lowerCase);
+  };
+
+  if (useUpperCase === true) {
+    passwordOptions.push.apply(passwordOptions, upperCase);
+  };
+
+  if (useNumbers === true) {
+    passwordOptions.push.apply(passwordOptions, numbers);
+  };
+
+  if (useSpecialCharacters === true) {
+    passwordOptions.push.apply(passwordOptions, specialCharacters);
+  };
+};
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
+
+function writePassword () {
+  passwordDisplay.innerHTML = "";
+  setPasswordLength();
+  defineCharacterTypes();
+  for (let i = 0; i < passwordLength; i++) {
+    shuffle(passwordOptions);
+    passwordDisplay.innerHTML = passwordDisplay.value + passwordOptions[0];
+  };
+};
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # Homework-03
+
+## Pseudocode
+
+variables: 
+
+    password length, upper case characters, lower case characters, numbers, special characters
+
+outcomes:
+
+    create a password between 8 and 128 characters
+
+    include lower case characters
+    exclude lower case characters
+
+    include UPPER case characters
+    exclude UPPER case characters
+    
+    include numbers
+    exclude numbers
+
+    include special characters
+    exclude special characters
+
+pseudocode:
+
+    create array containing all lower case letters
+    create array containing all upper case letters
+    create array containing all numbers
+    create array containing all special characters
+    create an emptry array that will represent the desired password length
+    create an empty array that will contain all allowed characters defined by prompts
+
+    create a function to set a password length between 8 and 128 characters
+
+        define the password length variable in a prompt. parse as integer
+
+        while the password length is less that 8 or greater than 128, send an alert that the input was invalid and resend the prompt to define password length
+
+    create a function to create an array with all allowed character types
+        confirm that the user wants the password to contain lower case, upper case, number, and special characters
+
+        while not allowing the use of any characters, alert the user that the password must contain some type of characters and confirm the character options again
+
+        if using lower case letters, add all array contents from lower case array to the allowed characters array
+        
+        if using upper case letters, add all array contents from upper case array to the allowed characters array
+
+        if using numbers, add all array contents from number array to the allowed characters array
+
+        if using special characters, add all array contents from special character array to the allowed characters array
+
+    create a function to shuffle the contents of an array
+
+    create a fucntion to write the password and display it in the text area
+
+        call the set password length function
+
+        call the allowed character type function
+
+        create a loop that starts at index 0 and continues to loop so long as the index remains less than the defined password length
+            shuffle the allowed password character array
+            in the password text area in the html, take the current text and add the first option in the array
+
+
+    set an event listener to run the write passsword function once the "generate password" button is clicked
